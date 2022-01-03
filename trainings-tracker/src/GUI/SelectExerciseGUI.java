a8efd3b98f7cc911e287982633a15b3cd92221e4
Giulia <giulia.ferraina@bluewin.ch>
2022-01-03 10:38:38 +0100

    edit and delet and add

--- trainings-tracker/src/GUI/SelectExerciseGUI.java
+++ trainings-tracker/src/GUI/SelectExerciseGUI.java
@@ -15,7 +15,22 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class SelectExerciseGUI extends JFrame implements ActionListener {
-
+	// create the buttons
+	private JButton deleteButton = new JButton("Delete");
+	private	JButton editButton = new JButton("Edit");
+	private JButton selectButton = new JButton("Select");
+	//create search button to search
+	private JButton searchButton = new JButton("Search");
+	// create search field
+	private JTextField searchField = new JTextField("");
+	// create add button to add a exercise		
+	private	JButton addButton = new JButton("Add");
+	// create input field, to add new exercise
+	private JTextField input = new JTextField();
+	// create a list of exercises
+	private JList<String> exercisesList = new JList<String>();
+	private DefaultListModel<String> model = new DefaultListModel<String>();
+	
 	public SelectExerciseGUI() {
 		// create window
 		this.setSize(500,600);
@@ -29,10 +44,6 @@ public class SelectExerciseGUI extends JFrame implements ActionListener {
 		// create panel for buttons
 		JPanel buttonsPanel = new JPanel();
 		buttonsPanel.setLayout(new GridLayout(0,3));
-		// create the buttons
-		JButton deleteButton = new JButton("Delete");
-		JButton editButton = new JButton("Edit");
-		JButton selectButton = new JButton("Select");
 		// add the buttons to the JPanel
 		buttonsPanel.add(deleteButton);
 		buttonsPanel.add(editButton);
@@ -45,19 +56,14 @@ public class SelectExerciseGUI extends JFrame implements ActionListener {
 		
 		// create search panel
 		JPanel searchPanel = new JPanel(new BorderLayout());
-		//create search button to search
-		JButton searchButton = new JButton("Search");
-		// create search field
-		JTextField searchField = new JTextField("");
+	
 		// add the search button and field to the search panel
 		searchPanel.add(searchButton, BorderLayout.EAST);
 		searchPanel.add(searchField, BorderLayout.CENTER);
 		// add the searchPanel to the exercise Panel
 		exercisePanel.add(searchPanel, BorderLayout.NORTH);
 		
-		// create a list of exercises
-		JList<String> exercisesList = new JList<String>();
-		DefaultListModel<String> model = new DefaultListModel<String>();
+		
 		model.addElement("Benchpress");
 		model.addElement("Squats");
 		model.addElement("Reverse flyes");
@@ -65,10 +71,7 @@ public class SelectExerciseGUI extends JFrame implements ActionListener {
 		//add the list to the exercise panel
 		exercisePanel.add(exercisesList, BorderLayout.CENTER);
 		
-		// create add button to add a exercise		
-		JButton addButton = new JButton("Add");
-		// create input field, to add new exercise
-		JTextField input = new JTextField();
+		
 		//create new Panel to add a new exercise
 		JPanel createExercisePanel = new JPanel(new BorderLayout());
 		createExercisePanel.add(addButton, BorderLayout.EAST);
@@ -80,13 +83,54 @@ public class SelectExerciseGUI extends JFrame implements ActionListener {
 		//add the exercisePanel to the window
 		this.add(exercisePanel, BorderLayout.CENTER);
 		
+		
+		deleteButton.addActionListener(this);
+		editButton.addActionListener(this);
+		addButton.addActionListener(this);
+		selectButton.addActionListener(this);
+		
+		
 		this.setVisible(true);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if (e.getSource() == addButton) {
+			model.addElement(input.getText());
+			input.setText("");
+		}
+		if(e.getSource() == deleteButton) {
+			int index = exercisesList.getSelectedIndex();
+			this.removeByIndex(index);
+		}
 		
+		if (e.getSource() == editButton) {
+			int index = exercisesList.getSelectedIndex();
+			input.setText(model.get(index));
+			this.removeByIndex(index);
+		}
+		if (e.getSource() == searchButton) {
+			/*
+			 for (String s :liste) {
+		            if (!s.startsWith(searchField.getText())) {
+		                if (model.contains(s)) {
+		                    model.removeElement(s);
+		                }
+		            } else {
+		                if (!model.contains(s)) {
+		                    model.addElement(s);
+		                }
+		            }
+		        }*/
+		}
+		
+		
+	}
+	private void removeByIndex(int index) {
+		if (index >=0) {
+			model.remove(index);
+		}
 	}
 
 }
